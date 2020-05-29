9bdecfeae6d9c9ce83a0421c744c58830831a3ff
goonhope <50105128+goonhope@users.noreply.github.com>
2020-05-29 08:49:53 +0800

    Update and rename test.cmd to pdf_compress_test.bat

--- pdf_compress_test.bat
+++ pdf_compress_test.bat
@@ -1,21 +1,17 @@
 @echo off
-title PDF compress by ghostscript:
-echo pdf compress by ghostscript:
-::-dColorImageResolution=100 == -r100 dQUIET == q
+title PDF compress
 
+echo pdf compress by ghostscript...
+::-dColorImageResolution=100 == -r100 dQUIET == q
 for %%i in (*.pdf) do (
 gswin64c -sDEVICE=pdfwrite -dCompatibilityLevel=1.7 -dDownsampleColorImages=true -r72 -dNOPAUSE  -q  -dBATCH -dEmbedAllFonts=false -dDownsampleColorImages=true -dDownsampleGrayImages=true -dDownsampleMonoImages=true  -dCompressFonts=true -sOutputFile=%%~dpni_gs.pdf  %%~fi
 )
 
-
-echo pdf compress by magick-mode-zip:
+echo pdf compress by magick-mode-zip...
 for /f %%i in ('dir /b *.pdf ^|find "_" /v ') do magick convert -compress zip %%~fi %%~dpni_magcik_zip.pdf
 
-
-echo pdf compress by magick-mode-jpeg:
+echo pdf compress by magick-mode-jpeg...
 for /f %%i in ('dir /b *.pdf ^|find "_" /v ')do magick convert -compress jpeg %%~fi %%~dpni_magcik_jpeg.pdf
 
-
-echo pdf info
+echo pdf info...
 for %%i in (*.pdf) do echo %%i=============== >> pdf.log & cpdf -info %%i >>pdf.log
-
